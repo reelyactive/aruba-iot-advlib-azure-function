b3b576f590ffdd8a0f795c5d3971177bd9e42024
jeffyactive <jeff@reelyactive.com>
2022-09-08 10:23:16 -0400

    Extract payloads from serial data messages

--- ProcessIoTHubMessages/index.js
+++ ProcessIoTHubMessages/index.js
@@ -1,5 +1,20 @@
+/**
+ * Copyright reelyActive 2022
+ * We believe in an open Internet of Things
+ */
+
 module.exports = function(context, IoTHubMessages) {
-  IoTHubMessages.forEach(message => { });
+  IoTHubMessages.forEach(messageString => {
+    let message = JSON.parse(messageString);
+
+    // Handle serialData
+    if(Array.isArray(message.serialData)) {
+      message.serialData.forEach(packet => {
+        let payload = Buffer.from(packet.data, 'base64');
+      });
+    }
+
+  });
 
   context.done();
 };
