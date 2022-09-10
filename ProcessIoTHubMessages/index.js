a83bf65913e215759cff5c4ee45908d2caffbc49
jeffyactive <jeff@reelyactive.com>
2022-09-10 09:37:07 -0400

    Added advlib-esp integration

--- ProcessIoTHubMessages/index.js
+++ ProcessIoTHubMessages/index.js
@@ -3,6 +3,13 @@
  * We believe in an open Internet of Things
  */
 
+
+const advlib = require('advlib-esp');
+
+
+const ADVLIB_OPTIONS = { ignoreProtocolOverhead: true };
+
+
 module.exports = function(context, IoTHubMessages) {
   IoTHubMessages.forEach(messageString => {
     let message = JSON.parse(messageString);
@@ -11,6 +18,7 @@ module.exports = function(context, IoTHubMessages) {
     if(Array.isArray(message.serialData)) {
       message.serialData.forEach(packet => {
         let payload = Buffer.from(packet.data, 'base64');
+        let processedPayload = advlib.process(payload, [], ADVLIB_OPTIONS);
       });
     }
 
