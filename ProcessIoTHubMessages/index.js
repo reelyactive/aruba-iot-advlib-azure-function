4ad0302c98204ae0dcabfca6890c85803dc55281
jeffyactive <jeff@reelyactive.com>
2022-09-16 09:28:22 -0400

    Output dynamb objects

--- ProcessIoTHubMessages/index.js
+++ ProcessIoTHubMessages/index.js
@@ -23,6 +23,33 @@ const ENOCEAN_PROCESSORS = [
 const INTERPRETERS = [ require('advlib-interoperable') ];
 
 
+// Default dynamic ambient (dynamb) properties to include in output messages
+const DEFAULT_DYNAMB_PROPERTIES = [
+    'acceleration',
+    'angleOfRotation',
+    'batteryPercentage',
+    'batteryVoltage',
+    'elevation',
+    'heading',
+    'heartRate',
+    'illuminance',
+    'interactionDigest',
+    'isButtonPressed',
+    'isContactDetected',
+    'isMotionDetected',
+    'magneticField',
+    'nearest',
+    'position',
+    'pressure',
+    'relativeHumidity',
+    'speed',
+    'temperature',
+    'txCount',
+    'unicodeCodePoints',
+    'uptime'
+];
+
+
 /**
  * Process IoT Hub Messages from Aruba APs.
  * @param {Object} context The Azure Function context.
@@ -35,10 +62,13 @@ module.exports = function(context, IoTHubMessages) {
     // Handle bleData
     if(Array.isArray(message.bleData)) {
       message.bleData.forEach(packet => {
+        let deviceId = ''; // TODO: from properties
+        let deviceIdType = ((packet.macAddrType === 'public') ? 2 : 3);
         let payload = Buffer.from(packet.data, 'base64');
         let processedPayload = advlib.process(payload, BLE_PROCESSORS,
                                               INTERPRETERS);
-        context.bindings.outputEventHubMessage = JSON.stringify(processedPayload);
+        let dynamb = compileDynamb(deviceId, deviceIdType, processedPayload);
+        context.bindings.outputEventHubMessage = JSON.stringify(dynamb);
       });
     }
 
@@ -47,7 +77,14 @@ module.exports = function(context, IoTHubMessages) {
       message.serialData.forEach(packet => {
         let payload = Buffer.from(packet.data, 'base64');
         let processedPayload = advlib.process(payload, ENOCEAN_PROCESSORS);
-        context.bindings.outputEventHubMessage = JSON.stringify(processedPayload);
+
+        if(Array.isArray(processedPayload.deviceIds)) {
+          let deviceIdElements = processedPayload.deviceIds[0].split('/');
+          let deviceId = deviceIdElements[0];
+          let deviceIdType = parseInt(deviceIdElements[1]);
+          let dynamb = compileDynamb(deviceId, deviceIdType, processedPayload);
+          context.bindings.outputEventHubMessage = JSON.stringify(dynamb);
+        }
       });
     }
 
@@ -55,3 +92,25 @@ module.exports = function(context, IoTHubMessages) {
 
   context.done();
 };
+
+
+/**
+ * Compile dynamic ambient (dynamb) data object from device id & data.
+ * @param {String} deviceId The device identifier.
+ * @param {Number} deviceIdType The type of device identifier (see raddec).
+ * @param {Object} data The processed payload data of the device.
+ * @return {Object} The compiled dynamb object.
+ */
+function compileDynamb(deviceId, deviceIdType, data) {
+  let dynamb = { deviceId: deviceId,
+                 deviceIdType: deviceIdType,
+                 timestamp: Date.now() };
+
+  for(const property in data) {
+    if(DEFAULT_DYNAMB_PROPERTIES.includes(property)) {
+      dynamb[property] = data[property];
+    }
+  }
+
+  return dynamb;
+}
